8a69db65d639a53a51e14b10e915f08a50220dad
Skeo <skeo051600@gmail.com>
2023-10-13 23:56:02 -0500

    Add styling and click clacks

--- main.js
+++ main.js
@@ -1,1 +1,89 @@
-console.log("test")
+console.log("test");
+
+const click = new Audio("./click.mp3")
+
+ 
+clear.addEventListener("click", function() {
+  click.play();
+  click.currentTime=0;
+});
+
+backspace.addEventListener("click", function() {
+  click.play();
+  click.currentTime=0;
+});
+
+divide.addEventListener("click", function() {
+  click.play();
+  click.currentTime=0;
+});
+
+seven.addEventListener("click", function() {
+  click.play();
+  click.currentTime=0;
+});
+
+eight.addEventListener("click", function() {
+  click.play();
+  click.currentTime=0;
+});
+
+nine.addEventListener("click", function() {
+  click.play();
+  click.currentTime=0;
+});
+
+times.addEventListener("click", function() {
+  click.play();
+  click.currentTime=0;
+});
+
+four.addEventListener("click", function() {
+  click.play();
+  click.currentTime=0;
+});
+
+five.addEventListener("click", function() {
+  click.play();
+  click.currentTime=0;
+});
+
+six.addEventListener("click", function() {
+  click.play();
+  click.currentTime=0;
+});
+
+subtract.addEventListener("click", function() {
+  click.play();
+  click.currentTime=0;
+});
+
+one.addEventListener("click", function() {
+  click.play();
+  click.currentTime=0;
+});
+
+two.addEventListener("click", function() {
+  click.play();
+  click.currentTime=0;
+});
+
+three.addEventListener("click", function() {
+  click.play();
+  click.currentTime=0;
+});
+
+addition.addEventListener("click", function() {
+  click.play();
+  click.currentTime=0;
+});
+
+zero.addEventListener("click", function() {
+  click.play();
+  click.currentTime=0;
+});
+
+equalsTo.addEventListener("click", function() {
+  click.play();
+  click.currentTime=0;
+});
